22fe498fe44e10807a5033d1e8f2c455643f1b13
stanislavcodes <korchevsky2004@gmail.com>
2020-04-28 15:46:07 +0300

    mobile nav and hero section

--- js/script.js
+++ js/script.js
@@ -1,13 +1,22 @@
 // mobile nav
-const headingPrimary = document.querySelector('.heading-primary');
+const body = document.getElementById("body");
+const headingBox = document.querySelector(".section-hero__heading-box");
 const nav = document.querySelector(".nav");
 const list = document.querySelector(".nav__list");
 const hamburger = document.querySelector(".nav__hamburger");
 
-hamburger.addEventListener("click", function () {
+// let navHeight = window.getComputedStyle(nav).height;
+
+
+hamburger.addEventListener("click", function (e) {
+  e.preventDefault();
+  body.classList.toggle("open");
   hamburger.classList.toggle("open");
   nav.classList.toggle("open");
   list.classList.toggle("open");
-  headingPrimary.classList.toggle("open");
-  console.log("ok");
+  headingBox.classList.toggle("open");
 });
+
+  // console.log("before: " + window.getComputedStyle(headingBox).padding);
+  // headingBox.style.padding = `calc(13.5vh + ${navHeight}) 2.5rem 0 2.5rem;`;
+  // console.log("after: " + window.getComputedStyle(headingBox).padding);
