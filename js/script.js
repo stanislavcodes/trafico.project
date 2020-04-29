f7adee380d95590e81ad89cedb679b4652035684
stanislavcodes <korchevsky2004@gmail.com>
2020-04-29 13:09:25 +0300

    nav fix

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/////////////////////// selectors
 // mobile nav
 const body = document.getElementById("body");
 const headingBox = document.querySelector(".section-hero__heading-box");
@@ -5,18 +6,19 @@ const nav = document.querySelector(".nav");
 const list = document.querySelector(".nav__list");
 const hamburger = document.querySelector(".nav__hamburger");
 
-// let navHeight = window.getComputedStyle(nav).height;
-
+/////////////////////// event listeners
 
 hamburger.addEventListener("click", function (e) {
+  navToggle(e);
+});
+
+/////////////////////// functions
+
+function navToggle(e) {
   e.preventDefault();
   body.classList.toggle("open");
   hamburger.classList.toggle("open");
   nav.classList.toggle("open");
   list.classList.toggle("open");
   headingBox.classList.toggle("open");
-});
-
-  // console.log("before: " + window.getComputedStyle(headingBox).padding);
-  // headingBox.style.padding = `calc(13.5vh + ${navHeight}) 2.5rem 0 2.5rem;`;
-  // console.log("after: " + window.getComputedStyle(headingBox).padding);
+}
